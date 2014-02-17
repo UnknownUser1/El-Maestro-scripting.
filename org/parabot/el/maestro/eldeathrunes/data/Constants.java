c9f82e7534dd20cb4f54b2f5ca392789eb099f12
el maestro <elmaestro989@gmail.com>
2014-02-17 14:27:50 -0600

    f

--- org/parabot/el/maestro/eldeathrunes/data/Constants.java
+++
@@ -1,7 +0,0 @@
-package org.parabot.el.maestro.eldeathrunes.data;
-
-public class Constants {
-    public static int ALTAR_ID = 2488;
-    public static int RUNE_ESS = 1436;
-    public static int DEATH_RUNE = 560;
-}
